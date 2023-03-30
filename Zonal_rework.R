2ae3280dddba9d338f98ab80bfd1623822484aa3
Thomas Estabrook <tgestab@umich.edu>
2023-03-30 12:15:06 -0700

    made initial files

---
+++ Zonal_rework.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(terra)
+library(tidyterra)
+library(lidR)
+library(sf)
+
+### Load in segmentation
+
+### Load in rasters
+### Add in LS8 and Sentinel2
+### Mask out non-forest pixels
+
+### Process additional indices
+### NDVI, EVI, LAI?, texture ratio, other textures
+
+### terra::extract for each raster
+
+
+
